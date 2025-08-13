22169e9af5268cf1a39f166e9c85708ed6f750a8
imamknitto <it14.knitto@gmail.com>
2025-08-13 14:47:59 +0700

    add hello world

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,7 @@ const App = () => {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+      <h1>Hello World</h1>
       <h1>VRE</h1>
       <p>Version: {version}</p>
     </>
